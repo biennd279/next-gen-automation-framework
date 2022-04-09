88a054348330535d736768ffc739232854c9451a
biennd279 <bien.uet279@gmail.com>
2022-04-09 16:34:54 +0700

    add more utils

--- src/main/kotlin/me/d3s34/sqlmap/restapi/TaskDataContentType.kt
+++ src/main/kotlin/me/d3s34/sqlmap/restapi/TaskDataContentType.kt
@@ -1,5 +1,8 @@
 package me.d3s34.sqlmap.restapi
 
+import me.d3s34.sqlmap.restapi.data.*
+import kotlin.reflect.KClass
+
 enum class TaskDataContentType(val id: Int) {
     TARGET(0),
     TECHNIQUES(1),
@@ -27,5 +30,30 @@ enum class TaskDataContentType(val id: Int) {
     FILE_WRITE(23),
     OS_CMD(24),
     REG_READ(25),
-    STATEMENTS(26),
+    STATEMENTS(26);
+
+    companion object {
+        private val map = values().associateBy(TaskDataContentType::id)
+        fun findById(id: Int): TaskDataContentType? = map[id]
+    }
+}
+
+fun TaskDataContentType.getClassType(): KClass<out AbstractData> {
+    return when (this.id) {
+        TaskDataContentType.TARGET.id -> TargetData::class
+        TaskDataContentType.DUMP_TABLE.id -> DumpTableData::class
+        TaskDataContentType.TECHNIQUES.id ->TechniqueData::class
+        TaskDataContentType.CURRENT_USER.id,
+        TaskDataContentType.CURRENT_DB.id,
+        TaskDataContentType.HOSTNAME.id ->StringData::class
+        TaskDataContentType.DBS.id ->ListStringData::class
+        TaskDataContentType.PRIVILEGES.id,
+        TaskDataContentType.ROLES.id,
+        TaskDataContentType.TABLES.id ->MapStringListStringData::class
+        TaskDataContentType.COLUMNS.id,
+        TaskDataContentType.SCHEMA.id ->MapDbToTableToString::class
+        TaskDataContentType.COUNT.id ->MapDbToCountToListTable::class
+        TaskDataContentType.SQL_QUERY.id ->ListToListStringData::class
+        else -> AbstractData::class
+    }
 }
