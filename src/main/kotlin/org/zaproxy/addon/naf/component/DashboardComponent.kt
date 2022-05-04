acbebcee517f2e26605836d0ddf63d3bebd5f73f
Bien Nguyen Dinh <bien.uet279@gmail.com>
2022-05-04 11:16:52 +0700

    improve alert, issue ui

--- src/main/kotlin/org/zaproxy/addon/naf/component/DashboardComponent.kt
+++ src/main/kotlin/org/zaproxy/addon/naf/component/DashboardComponent.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.State
 import com.arkivanov.decompose.ComponentContext
 import org.zaproxy.addon.naf.NafScan
 import org.zaproxy.addon.naf.NafState
+import org.zaproxy.addon.naf.model.NafAlert
 
 class DashboardComponent(
     componentContext: ComponentContext,
     nafState: NafState,
-    val currentScan: State<NafScan?>
+    val currentScan: State<NafScan?>,
+    val sendAlert: (NafAlert) -> Unit
 ): ComponentContext by componentContext, NafState by nafState {
 
 }
